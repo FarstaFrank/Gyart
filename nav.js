fcdde4b322fdc65f5e2c5130c2d3bad40b7a15ea
FarstaFrank <frank@petrolheads.se>
2024-01-10 12:00:59 +0100

    yes2

--- nav.js
+++ nav.js
@@ -1,11 +1,13 @@
 var home = document.getElementById("home");
 var textBilde = document.getElementById("textBild");
+var chat = document.getElementById("chat");
 
 var openHome = document.getElementById("openHome");
 var openTextBild = document.getElementById("openTextBild");
+var openchat = document.getElementById("openChat");
 
-var curentWindow = "home";
-open;
+var curentWindow = "textBild";
+open();
 
 openHome.addEventListener("click", function () {
     curentWindow = "home";
@@ -13,6 +15,10 @@ openHome.addEventListener("click", function () {
 });
 openTextBild.addEventListener("click", function () {
     curentWindow = "textBild";
+    open();
+});
+openChat.addEventListener("click", function () {
+    curentWindow = "chat";
     open();
 });
 
@@ -23,14 +29,22 @@ function open(){
         case "home":
             home.style.display = "block";
             textBilde.style.display = "none";
+            chat.style.display = "none";
             break;
         case "textBild":
             textBilde.style.display = "block";
             home.style.display = "none";
+            chat.style.display = "none";
+            break;
+        case "chat":
+            textBilde.style.display = "none";
+            home.style.display = "none";
+            chat.style.display = "block";
             break;
         default:
             home.style.display = "block";
             textBilde.style.display = "none";
+            home.style.display = "none";
             break;
     }
 }
